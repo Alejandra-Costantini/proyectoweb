4612e54768f86bcf8a85c65a2557981140f951b3
Alejandra-Costantini <acostant@agro.uba.ar>
2022-10-28 20:59:58 -0300

    ProyectoWebFinal

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 fetch('https://randomuser.me/api/')
-.then(res => res.json())
-.then(data => {
-    console.log(data.results['0'])
-    contenido.innerHTML = `
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.results['0'])
+        contenido.innerHTML = `
 <img src="${data.results['0'].picture.large}">
 <p>Username: "${data.results['0'].login.username}"</p>
 <p>Email: "${data.results['0'].email}"</p>`
 
-})
+    })
 
+function validateEmail() {
+    var emailField = document.getElementById('email-contacto')
+    var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w)*(\.\w{2, 10})+$/;
+
+    if (validEmail.test(emailField.value)) {
+        alert('Email valido');
+        return true;
+    } else {
+        alert('Email invalido');
+        return false
+    }
+}
